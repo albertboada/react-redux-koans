6b9fa72379b553180eb690099e406c867cc8dae1
Marc Rivelles <rivelles.marc@gmail.com>
2016-12-15 13:54:32 +0000

    Reducers Fixed

--- src/reducers/beers.js
+++ src/reducers/beers.js
@@ -6,10 +6,12 @@ reducers should do 3 things:
     2. Handle an action by checking the type and return a new state WITHOUT mutating the state
     3. Return the default state if no action is handled
 */
-const beers = (state, action) => {
+const beers = (state = [], action) => {
   switch (action.type) {
-    case 'CERVEZA':
-      return [...state]
+    case RECEIVE_BEERS:
+      return [...state, ...action.beers]
+    default:
+      return state
   }
 }
 
