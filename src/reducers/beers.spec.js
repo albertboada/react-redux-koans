d02797e8f19cdd09e4061f7b812a1e43fa368957
Marc Rivelles <rivelles.marc@gmail.com>
2016-12-15 14:27:03 +0000

    Re order tests

--- src/reducers/beers.spec.js
+++ src/reducers/beers.spec.js
@@ -2,17 +2,17 @@ import { expect } from 'chai'
 import beersReducer from './beers'
 
 describe("The beers reducer in src/reducers/beers.js", () => {
+  it("should return the default state by default if it doesn't match any action", () => {
+    const state = beersReducer([5], {type: 'test'})
+
+    expect(state).to.be.deep.equal([5])
+  })
+  
   it("should set the default state in the parameter called state", () => {
     const state = beersReducer(undefined, {type: 'test'})
 
     expect(state).not.to.be.equal(undefined)
     expect(Object.prototype.toString.call(state)).to.be.equal('[object Array]')
-  })
-  
-  it("should return the default state by default if it doesn't match any action", () => {
-    const state = beersReducer([5], {type: 'test'})
-
-    expect(state).to.be.deep.equal([5])
   })
 
   it("should handle the action type called RECEIVE_BEERS and add the new beers at the end of the array in the state without mutating the array", () => {
