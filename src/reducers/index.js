351ff915df7ea6abfaa6c58201806191458d72d0
Marc Rivelles <rivelles.marc@gmail.com>
2016-12-15 14:32:34 +0000

    Combine reducers handmade

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
+import { combineReducers } from '../redux/combineReducers'
 
 import beers from './beers'
 import cart from './cart'
