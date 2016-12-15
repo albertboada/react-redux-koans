83d835544dc6d82366377e658ca4ee301a4bb8db
alexlbr <alex.lobera@gmail.com>
2016-12-15 11:55:33 +0000

    added comments to beer reducer

--- src/reducers/beers.js
+++ src/reducers/beers.js
@@ -1,5 +1,11 @@
 import { RECEIVE_BEERS } from '../actions'
 
+/*
+reducers should do 3 things:
+    1. Set the default state
+    2. Handle an action by checking the type and return a new state WITHOUT mutating the state
+    3. Return the default state if no action is handled
+*/
 const beers = (state, action) => {
   switch (action.type) {
     case 'HOLA':
