ce5a85056591b8174184c6eaedf656389a6454ba
Marc Rivelles <rivelles.marc@gmail.com>
2016-12-15 15:47:29 +0000

    Update Cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -4,8 +4,9 @@ const cart = (state = [], action) => {
   switch (action.type) {
     case ADD_BEER_TO_CART:
       return [...state, action.beer]
+    default:
+      return state
   }
-  return state
 }
 
 export default cart
