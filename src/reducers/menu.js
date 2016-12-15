2cc5b41565fbfe7fbc6cb9efcdbff1725c9d0cc2
Marc Rivelles <rivelles.marc@gmail.com>
2016-12-15 16:05:17 +0000

    Step 4 Done

--- src/reducers/menu.js
+++ src/reducers/menu.js
@@ -1,9 +1,11 @@
-import { TOGGLE_SIDE_MENU } from '../actions'
+import { TOGGLE_SIDE_MENU, ADD_BEER_TO_CART } from '../actions'
 
 const menu = (state = { isOpen: false }, action) => {
   switch (action.type) {
     case TOGGLE_SIDE_MENU:
       return { ...state, isOpen: !state.isOpen }
+    case ADD_BEER_TO_CART:
+      return { ...state, isOpen: true }
     default:
       return state
   }
