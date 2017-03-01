9a9c40aebf24d1955a16673a3bc1191b4932660d
Albert Boada <boa.intrance@gmail.com>
2017-03-02 00:06:26 +0100

    Fixes Action Creator.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 export const RECEIVE_BEERS = 'RECEIVE_BEERS'
 export const ADD_BEER_TO_CART = 'ADD_BEER_TO_CART'
 
-export const receiveBeers = beers => ({
-  type: 'HOW_SHOULD_I_BE_CALLED?',
-})
+export const receiveBeers = (beers) => ({
+  type: RECEIVE_BEERS,
+  beers
+});
 
 export const addBeerToCart = beer => ({
   type: ADD_BEER_TO_CART,
