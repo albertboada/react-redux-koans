aca4e2ec9159d02b43e6ba2e99e24c257e0184d3
Albert Boada <boa.intrance@gmail.com>
2017-03-02 00:40:33 +0100

    When a beer is added to the basket the side menu with the basket opens.

--- src/reducers/isSideMenuOpen.js
+++ src/reducers/isSideMenuOpen.js
@@ -1,9 +1,11 @@
-import { TOGGLE_SIDE_MENU } from '../actions';
+import { TOGGLE_SIDE_MENU, ADD_BEER_TO_CART } from '../actions';
 
 const app = (state = false, action) => {
   switch (action.type) {
     case TOGGLE_SIDE_MENU:
       return !state;
+    case ADD_BEER_TO_CART:
+      return true;
     default:
       return state;
   }
